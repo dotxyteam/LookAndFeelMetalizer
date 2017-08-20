40f89d461903620ea62f7a067fe5932171882e11
olitank <olitank@olitank-MS-7846>
2017-08-20 20:12:16 +0200

    regression test added

---
+++ src/test/java/xy/lib/theme/TestThemeEqualization.java
@@ -0,0 +1,36 @@
+package xy.lib.theme;
+
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestThemeEqualization {
+
+	@Test
+	public void test() {
+		final List<Throwable> errors = new ArrayList<Throwable>();
+		final ThemeEqualizerDialog dialog = new ThemeEqualizerDialog(null, new EqualizedTheme(100, 100, 100));
+		try {
+			dialog.addWindowListener(new WindowAdapter() {
+				@Override
+				public void windowOpened(WindowEvent e) {
+					try {
+						dialog.dispose();
+					} catch (Throwable t) {
+						errors.add(t);
+					}
+				}
+			});
+		} catch (Throwable t) {
+			errors.add(t);
+		}
+		if (errors.size() > 0) {
+			Assert.fail(errors.toString());
+		}
+	}
+
+}
